f53f4a14876689f9c8f4a75fdd017657625cd9a4
tombetthauser <tombetthauser@gmail.com>
2020-11-18 17:03:39 -0800

    Add academic approach

--- scratch_files/vector-validator.js
+++ scratch_files/vector-validator.js
@@ -55,6 +55,33 @@ console.log(validator(square, [-1,-1])) // true
 console.log(validator(square, [-3,-1])) // false
 
 
+// JavaScript approach by M. Katz based on W. Randolph Franklin's approach (Rensselaer Polytechnic Institute).
+// https://stackoverflow.com/questions/217578/how-can-i-determine-whether-a-2d-point-is-within-a-polygon
+// https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html
 
+// function pointIsInPoly(p, polygon) {
+//     var isInside = false;
+//     var minX = polygon[0].x, maxX = polygon[0].x;
+//     var minY = polygon[0].y, maxY = polygon[0].y;
+//     for (var n = 1; n < polygon.length; n++) {
+//         var q = polygon[n];
+//         minX = Math.min(q.x, minX);
+//         maxX = Math.max(q.x, maxX);
+//         minY = Math.min(q.y, minY);
+//         maxY = Math.max(q.y, maxY);
+//     }
 
+//     if (p.x < minX || p.x > maxX || p.y < minY || p.y > maxY) {
+//         return false;
+//     }
 
+//     var i = 0, j = polygon.length - 1;
+//     for (i, j; i < polygon.length; j = i++) {
+//         if ( (polygon[i].y > p.y) != (polygon[j].y > p.y) &&
+//                 p.x < (polygon[j].x - polygon[i].x) * (p.y - polygon[i].y) / (polygon[j].y - polygon[i].y) + polygon[i].x ) {
+//             isInside = !isInside;
+//         }
+//     }
+
+//     return isInside;
+// }
